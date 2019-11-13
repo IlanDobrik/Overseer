a8ec27a39c0102783a6a76ebc92b17b6a7c10171
IlanDobrik <ilan.dobrik@gmail.com>
2019-11-13 12:34:54 +0200

    added site

--- boss.py
+++ boss.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from klein import run, route
 import socket
 import json
 import datetime
@@ -9,29 +10,14 @@ LISTEN_PORT = 1313
 SETTINGS = r"C:\Users\magshimim\Desktop\settings.dat"
 INCOMING = {}
 OUTGOING = {}
+data = []
 
 # site related
-class Site(BaseHTTPRequestHandler):
-	def do_GET(self):
-		self.send_response(200)
-		self.wfile.write(bytes("Test", "utf-8"))
+@route('/')
+def home(request):
+    return "XD"
 
-def run_site():
-    hostName = ""
-    hostPort = 80
-
-    # running server
-    site = HTTPServer((hostName, hostPort), Site)
-    print(time.asctime(), "Website Starts - %s:%s" % (hostName, hostPort))
-    try:
-        site.serve_forever()
-    except:
-        pass
-    # closing server
-    site.server_close()
-    print(time.asctime(), "Website Stops - %s:%s" % (hostName, hostPort))
-
-def country_traffic(data):
+def country_traffic():
     dict = {}
     for pack in data:
         if pack["locationIp"] == "None":
@@ -42,7 +28,7 @@ def country_traffic(data):
             dict[pack["locationIp"]] += pack["sizeOfPacket"]
     print(dict)
 
-def dstip_traffic(data):
+def dstip_traffic():
     dict = {}
     for pack in data:
         if pack["dstIp"] not in dict:
@@ -51,7 +37,7 @@ def dstip_traffic(data):
             dict[pack["dstIp"]] += pack["sizeOfPacket"]
     print(dict)
 
-def program_traffic(data):
+def program_traffic():
     dict = {}
     for pack in data:
         if pack["program"] not in dict:
@@ -60,7 +46,7 @@ def program_traffic(data):
             dict[pack["program"]] += pack["sizeOfPacket"]
     print(dict)
 
-def port_traffic(data):
+def port_traffic():
     dict = {}
     for pack in data:
         if pack["remotePort"] not in dict:
@@ -69,7 +55,7 @@ def port_traffic(data):
             dict[pack["remotePort"]] += pack["sizeOfPacket"]
     print(dict)
 
-def agent_traffic(data, worker):
+def agent_traffic(worker):
     byte_sum = 0
     for pack in data:
         if pack["outOrIn"]:
@@ -87,7 +73,7 @@ def agent_traffic(data, worker):
     else:
         INCOMING[worker] += byte_sum
 
-def get_msg():
+def get_msg_UDP():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_address = ('', LISTEN_PORT)
     sock.bind(server_address)
@@ -97,21 +83,53 @@ def get_msg():
     sock.close()
     return client_msg.decode(), client_addr
 
-def main():
-    global INCOMING, OUTGOING
+class ClientThread(threading.Thread):
+    def __init__(self, clientAddress, clientsocket):
+        threading.Thread.__init__(self)
+        self.csocket = clientsocket
+        self.caddress = clientAddress
 
-    print("started")
+    def run(self):
+        global data
+        print (self.caddress, "connected")
+
+        while True:
+            try:     
+                packs = self.csocket.recv(2048).decode()
+                data += json.loads(packs)
+            except Exception as e:
+                print(e)
+                break
+        print (self.caddress , "disconnected")
+
+def listen4clients():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(('', LISTEN_PORT))
+    print("Server started")
+    
     while True:
-        INCOMING = {}
-        OUTGOING = {}
-        data = []
+        server.listen(1)
+        clientsock, clientAddress = server.accept()
+        newthread = ClientThread(clientAddress, clientsock)
+        newthread.start()
+
+def main():
+    #run("localhost", 80)
+    t = threading.Thread(target=listen4clients, daemon=True)
+    t.start()
+
+    while True:
+        INCOMING.clear()
+        OUTGOING.clear()
+        data.clear()
 
         while len(data) < 5:
-            client_msg, client_addr = get_msg()
-            data += json.loads(client_msg)
-            agent_traffic(json.loads(client_msg), client_addr[0])
-            print(data)
-
+            time.sleep(1)
+        print("Writing")
+        with open("./output.txt", 'w') as f:
+            f.write(json.dumps(data))
+        #upload to site    
 
 if __name__ == '__main__':
     main()
