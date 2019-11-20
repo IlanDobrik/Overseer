67f01da3035c01d6a87f64bc23895319c363c2f7
IlanDobrik <ilan.dobrik@gmail.com>
2019-11-20 13:35:27 +0200

    fixed flask website

--- server/website.py
+++ server/website.py
@@ -3,23 +3,24 @@ import sys
 import logging
 from flask import Flask
 
+PAGE_OUT = sys.argv[1]
+app = Flask(__name__)
+
 @app.route('/')
-def home(request):
-    print(request)
+def home():
     reports = [x for x in os.listdir(PAGE_OUT) if ".html" in x]
     page = "<h2>this</h2>\n"
     for report in reports:
         page += '<a href="{}">{}</a>\n'.format(PAGE_OUT + report, report[: report.find(".html")])
     return page
-    
+
 @app.route('/pages/<string:name>')
-def reportPage(request, name):
-    print(request, name)
+def reportPage(name):
     with open(PAGE_OUT + name, 'r') as file:
         return file.read()
     return "An error has occurred"
 
-PAGE_OUT = sys.argv[1]
-app = Flask(__name__)
-log = logging.getLogger('werkzeug')
-log.disabled = True
+if __name__ == "__main__":
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
+    app.run(host="127.0.0.1", port=80)
